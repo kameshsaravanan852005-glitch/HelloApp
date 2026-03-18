3993d7777fda7502f3cda5bff202358c010fede2
kameshsaravanan852005-glitch <kameshsaravanan852005@gmail.com>
2026-03-18 11:03:08 +0530

    UC2: Display Hello with command line argument

---
+++ HelloApp.java
@@ -0,0 +1,12 @@
+public class HelloApp {
+    public static void main(String[] args) {
+
+        // Check if name is passed
+        if (args.length > 0) {
+            System.out.println("Hello " + args[0]);
+        } else {
+            System.out.println("Hello User");
+        }
+
+    }
+}
