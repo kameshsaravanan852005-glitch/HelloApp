8dabd539a5eb6cce38dca45362aadb774b3a75ba
kameshsaravanan852005-glitch <kameshsaravanan852005@gmail.com>
2026-03-24 21:15:56 +0530

    UC3: Display Hello with command line argument or default World

--- HelloApp.java
+++ HelloApp.java
@@ -1,11 +1,10 @@
 public class HelloApp {
     public static void main(String[] args) {
 
-        // Check if name is passed
         if (args.length > 0) {
             System.out.println("Hello " + args[0]);
         } else {
-            System.out.println("Hello User");
+            System.out.println("Hello World");
         }
 
     }
