a93961be0341e9091128e3ff02ec6177b9ed4cab
kameshsaravanan852005-glitch <kameshsaravanan852005@gmail.com>
2026-03-28 00:36:17 +0530

    uc7 added

--- StepHelloApp/src/main/java/org/example/HelloAppUC6.java
+++ StepHelloApp/src/main/java/org/example/HelloAppUC6.java
@@ -1,4 +1,23 @@
 package org.example;
 
-public class HelloAppUC6 {
+package org.example;
+
+public class HelloApp {
+    public static void main(String[] args) {
+
+        if (args.length > 0) {
+            String result = "Hello ";
+
+            for (String name : args) {
+                result += name + ", ";
+            }
+
+
+            result = result.substring(0, result.length() - 2);
+
+            System.out.println(result);
+        } else {
+            System.out.println("Hello, World!");
+        }
+    }
 }
---
+++ StepHelloApp/src/main/java/org/example/HelloAppUC7.java
@@ -0,0 +1,16 @@
+package org.example;
+
+
+
+public class HelloApp {
+    public static void main(String[] args) {
+
+        if (args.length > 0) {
+            // Join all names with comma
+            String names = String.join(", ", args);
+            System.out.println("Hello " + names);
+        } else {
+            System.out.println("Hello, World!");
+        }
+    }
+}
